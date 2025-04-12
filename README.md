c96a3a121f5f398fc821361a843bb8b7d23acf19
rdjaneiro <rodrigodejaneiro@gmail.com>
2025-04-12 15:40:46 +0000

    Add GNU General Public License v3.0 to project files and update README with license information

--- README.md
+++ README.md
@@ -86,4 +86,4 @@ If you encounter issues with extraction:
 
 ## License
 
-[Insert License Information]
+This project is licensed under the GNU General Public License v3.0 - see the [LICENSE](LICENSE) file for details.
--- dev_run.py
+++ dev_run.py
@@ -1,3 +1,20 @@
+"""
+Empower Portfolio WebArchive Extractor
+Copyright (C) 2025 Rodrigo Loureiro
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 #!/usr/bin/env python3
 import argparse
 import subprocess
@@ -6,7 +23,7 @@ import sys
 def run_streamlit_dev():
     print("Starting Streamlit in development mode on port 8502...")
     subprocess.run([
-        "streamlit", "run", 
+        "streamlit", "run",
         "streamlit_app.py",
         "--server.port", "8502",
         "--server.address", "localhost"
@@ -27,9 +44,9 @@ if __name__ == "__main__":
         choices=["streamlit", "dash"],
         help="Which app to run in development mode"
     )
-    
+
     args = parser.parse_args()
-    
+
     try:
         if args.app == "streamlit":
             run_streamlit_dev()
